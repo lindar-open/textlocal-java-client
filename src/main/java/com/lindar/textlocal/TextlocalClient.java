351b1a93dc6e8c2c8668238614cebe61b3e46caa
steven509 <steven.hills@gmail.com>
2018-01-04 17:15:33 +0000

    Added reporting resource to client

--- src/main/java/com/lindar/textlocal/TextlocalClient.java
+++ src/main/java/com/lindar/textlocal/TextlocalClient.java
@@ -3,12 +3,19 @@ package com.lindar.textlocal;
 public class TextlocalClient {
 
     private final ContactResource contactResource;
+    private final ReportingResource reportingResource;
+
 
     public TextlocalClient(String apiKey) {
         this.contactResource = new ContactResource(apiKey);
+        this.reportingResource = new ReportingResource(apiKey);
     }
 
     public ContactResource contacts() {
         return contactResource;
+    }
+
+    public ReportingResource reporting() {
+        return reportingResource;
     }
 }
